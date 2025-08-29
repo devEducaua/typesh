e22fecb7a5ef5d61cb1ce8f0590f37351ca465ad
devEducaua <schokito4@gmail.com>
2025-08-28 21:51:15 -0300

    feat: add the basics, but with some bugs

---
+++ main.ts
@@ -0,0 +1,54 @@
+import { cwd } from "node:process";
+import { exec } from "node:child_process";
+import readline from "node:readline";
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+async function readCmd(cmd: string) {
+    return new Promise(resolve => {
+        rl.question(cmd, result => resolve(result))
+    })
+}
+
+function execCmd(result: string) {
+     exec(result, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`ERROR: ${error.message}`);
+            return;
+        }
+
+        if (stderr) {
+            console.error(`STDERR: ${stderr}`);
+            return;
+        }
+
+        console.log(`${stdout}`);
+        return;
+    })   
+}
+
+async function main() {
+    
+    let quit: boolean = false;
+
+    while(!quit) {
+        const result = await readCmd(`${cwd()} $ `);
+
+        switch(result) {
+            case "exit":
+                quit = true;
+                break;
+            default:
+                execCmd(result);
+                break;
+        }
+    }
+
+    rl.close();
+    process.exit(0);
+}
+
+main();
